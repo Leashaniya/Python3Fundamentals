084053751968c8d08ff0e279bab57ec6179dfbfd
Leashaniya <leashakrish2002@gmail.com>
2023-12-15 10:38:19 +0530

    deleted 2 files

--- nestedcontacts.py
+++
@@ -1,9 +0,0 @@
-contacts={
-        'number':3,
-        'students':[
-                   { 'name':'sarah','email':'sarah@gmail.com'},
-                   {'name':'leasha','email':'leasha@gmail.com'},
-                   {'name':'meena','email':'meena@gmail.com'}
-
-        ]
-}
