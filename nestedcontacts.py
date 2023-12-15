37b5c224bb12db1a0dc4e92b6f7c01789174a600
Leashaniya <leashakrish2002@gmail.com>
2023-12-15 10:22:19 +0530

    Create nestedcontacts.py

---
+++ nestedcontacts.py
@@ -0,0 +1,9 @@
+contacts={
+        'number':3,
+        'students':[
+                   { 'name':'sarah','email':'sarah@gmail.com'},
+                   {'name':'leasha','email':'leasha@gmail.com'},
+                   {'name':'meena','email':'meena@gmail.com'}
+
+        ]
+}
