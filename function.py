69c21f8a59bab119f37cd409733f0e295aed9bfe
Leashaniya <leashakrish2002@gmail.com>
2023-12-17 00:13:09 +0530

    Update function.py

--- function.py
+++ function.py
@@ -4,8 +4,24 @@ def greeting(name):
 
 #greeting function name 
 #name is the paramter 
+#name in local scope
 
-#main program
-input_name=input("enter your name: ")
+#main program using local scope
+input_name1=input("enter your name: ")
+input_name2=input("enter your name: ")
 
-greeting(input_name) 
+greeting(input_name1)
+greeting(input_name2) 
+
+
+#if name is in global scope
+
+def greetings():
+    print("hi ",name1)
+
+name1=input("enter your name: ")
+greetings()
+name2=input("enter your name: ")
+name1=name2
+
+greetings()
