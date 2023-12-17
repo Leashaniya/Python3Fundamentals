0bc64da3e109163301d5ab4ae90da87e5c733ac2
Leashaniya <leashakrish2002@gmail.com>
2023-12-17 22:05:36 +0530

    Create robotdog.py

---
+++ robotdog.py
@@ -0,0 +1,25 @@
+
+class robotdog:
+    def __init__(self,name,breed):       #name and breed are properties
+        self.name=name
+        self.breed=breed
+
+dog=robotdog('jimmy','anybreed')
+print(dog.name)
+print(dog.breed)
+
+#now gonna add behaviors
+#creating a class method
+#we create a class method just like a function Except a method has self as the first parameter
+
+class robot_dog:
+    def __init__(self,name,breed):
+        self.name=name
+        self.breed=breed
+    def bark(self):                         #bark is a method or behavior
+        print("woof woof")
+
+doggy=robot_dog('jimmy','anybreed')
+# print(doggy.name)
+# print(doggy.breed)
+doggy.bark()
