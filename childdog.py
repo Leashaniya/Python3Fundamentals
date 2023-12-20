d923b502eaf250fac4ee251fea9e96427fa2c0f8
Leashaniya <leashakrish2002@gmail.com>
2023-12-20 20:45:16 +0530

    updated

--- childdog.py
+++ childdog.py
@@ -6,7 +6,18 @@ class dog(robot):
 
     def makenoise(self):
         print("woof woof")
+    
+    # def walk(self, y):              #just to check whether the parent class or the child class works first
+    #     print('hey')
+        
+    def eat(self):
+        # super().eat()
+        print("i like bacon")
 
 germanshepherd=dog('jimmy')
 germanshepherd.makenoise()
 germanshepherd.walk(5)
+#method overriding
+germanshepherd.eat()
+#if we wanted to call the parent class as well uncomment the super method line
+
