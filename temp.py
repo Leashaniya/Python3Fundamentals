f9cc5b0c1381ffe234dc698866d9b6a1ba8531a5
Leashaniya <leashakrish2002@gmail.com>
2023-12-23 16:55:59 +0530

    updated

---
+++ temp.py
@@ -0,0 +1,10 @@
+#ask the user what acronym they want to add
+#then ask the user for the definition
+#open the file
+#write the new acronym and definition to the file 
+
+acronym=input("what acronym you want to add \n")
+definition=input("what is the definition \n")
+
+with open ('workingfile.txt ','a') as file:
+    file.write(acronym+' - '+definition+'\n')
